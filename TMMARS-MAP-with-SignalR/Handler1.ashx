fd2b9b281c4eef202bfa9bd8186429f91b3c44c0
RichardHan <qwweee7467@gmail.com>
2015-08-18 11:31:07 +0800

    add handler sample.

---
+++ TMMARS-MAP-with-SignalR/Handler1.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="Handler1.ashx.cs" Class="TMMARS_MAP_with_SignalR.Handler1" %>
---
+++ TMMARS-MAP-with-SignalR/Handler1.ashx.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TMMARS_MAP_with_SignalR
+{
+    /// <summary>
+    /// Summary description for Handler1
+    /// </summary>
+    public class Handler1 : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var hubContext = GlobalHost.ConnectionManager.GetHubContext<TMMARS_MAP_with_SignalR.MARSHub>();
+            hubContext.Clients.All.alertFromServer("Test");
+
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
